e9ded4f52ecf1a18c6a1f8bdbd94a81503bb15ab
Guilherme <guilherme@exla.com.br>
2014-02-18 13:23:49 -0300

    marotao

--- test/example.js
+++ test/example.js
@@ -3,9 +3,12 @@ var test = require('tape');
 
 
 test('simples comparacao', function(t){
-
+    //ta ficando maroto
 	t.plan(1);
 
 	var n = maxBy([9,3,4], function (x){return x % 3 });
 	t.equal(n,4);
 })
+
+
+
